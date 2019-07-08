6b9cf6e73007d1ad43f476e23edcb9d1969b961a
lqt123l <331737204@qq.com>
2019-07-08 15:49:24 +0930

    Public server

--- src/actions/index.js
+++ src/actions/index.js
@@ -22,7 +22,7 @@ export const fetchProducts = (filter) => (dispatch, getState) => {
 
     const searchProducts = getState().searchProducts.split(" ").join("-").toLowerCase();;
 
-    return Axios.get(`${localHost5001}/product/${searchProducts}`)
+    return Axios.get(`${heroKuServer}/product/${searchProducts}`)
         .then(
             response => {
                 dispatch({
@@ -66,7 +66,7 @@ export const testSend = (value) => {
 
 export const fetchStores = () => (dispatch, getState) => {
     // return Axios.get('https://thawing-inlet-79899.herokuapp.com/store')
-    return Axios.get(`${localHost5001}/store`)
+    return Axios.get(`${heroKuServer}/store`)
         .then(
             response => {
                 console.log(response.data);
@@ -108,7 +108,7 @@ export const fetchStores = () => (dispatch, getState) => {
 export const sendProduct = (value) => {
     console.log(value);
     // Axios.post('https://thawing-inlet-79899.herokuapp.com/product', {
-    Axios.post(`${localHost5001}/product`, {
+    Axios.post(`${heroKuServer}/product`, {
         productName: value.productName,
         productBrand: value.productBrand,
         storeId: value.productStore,
