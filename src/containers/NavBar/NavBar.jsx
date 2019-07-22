ea7adc3058d832535c477dcdef396b545ceb90d0
lqt123l <331737204@qq.com>
2019-07-22 14:13:21 +0930

    toggle

--- src/containers/NavBar/NavBar.jsx
+++ src/containers/NavBar/NavBar.jsx
@@ -15,30 +15,44 @@ const LogoutBtn = styled.button`
 `
 
 class NavBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapsed: true,
+        };
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+    }
+    toggleNavbar() {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    }
     render() {
-        const {loggedIn,username,showLoginForm,showRegisterForm,logout} = this.props;
+        const { loggedIn, username, showLoginForm, showRegisterForm, logout } = this.props;
+        const collapsed = this.state.collapsed;
+        const toggleClass = collapsed ? 'collapse navbar-collapse' : 'collapse navbar-collapse show';
         return (
             <div className='container'>
                 <nav className="row navbar navbar-expand-lg navbar-light bg-light">
                     <NavLink className="navbar-brand" to='/'>Product Search</NavLink>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onClick={()=>this.toggleNavbar()}>
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <div className={toggleClass} id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item active">
                                 <NavLink className="dropdown-item" to='/product-manage'>Manage Product</NavLink>
                             </li>
-                            {!loggedIn&&<li className="nav-item active">
-                                <NavTag className="dropdown-item" onClick={()=>showLoginForm()}>Login</NavTag>
+                            {!loggedIn && <li className="nav-item active">
+                                <NavTag className="dropdown-item" onClick={() => showLoginForm()}>Login</NavTag>
                             </li>}
-                            
-                            {!loggedIn&&<li className="nav-item active">
-                                <NavTag className="dropdown-item" onClick={()=>showRegisterForm()}>Register</NavTag>
+
+                            {!loggedIn && <li className="nav-item active">
+                                <NavTag className="dropdown-item" onClick={() => showRegisterForm()}>Register</NavTag>
                             </li>}
                         </ul>
                     </div>
-                    {loggedIn && <div>Welcome! <UserInfoBtn>{username}</UserInfoBtn><LogoutBtn onClick={()=>logout()}>Logout</LogoutBtn></div>}
+                    {loggedIn && <div>Welcome! <UserInfoBtn>{username}</UserInfoBtn><LogoutBtn onClick={() => logout()}>Logout</LogoutBtn></div>}
                 </nav>
             </div>
 
@@ -48,8 +62,8 @@ class NavBar extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        loggedIn:state.login.loginStatus.loggedIn,
-        username:state.userInformation.username
+        loggedIn: state.login.loginStatus.loggedIn,
+        username: state.userInformation.username
     }
 }
 
